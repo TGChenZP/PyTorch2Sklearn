c2d60ef56d77dfc6f81e103245cac254d27eacf4
Lang (Ron) Chen <79613807+TGChenZP@users.noreply.github.com>
2024-09-04 22:32:07 +1000

    Update data.py

--- PyTorch2Sklearn/utils/data.py
+++ PyTorch2Sklearn/utils/data.py
@@ -2,21 +2,23 @@ from PyTorch2Sklearn.environment import *
 
 
 class TabularDataset(Dataset):
-
-    """ Creates list of instances for tabular data from pandas dataframe """
+    """Creates list of instances for tabular data from pandas dataframe"""
 
     def __init__(self, x_list, y_list=None):
         """
 
-            Input:
-                - x_list: list of features
-                - y_list: list of targets
+        Input:
+            - x_list: list of features
+            - y_list: list of targets
 
         """
 
         self.features = torch.tensor(np.array(x_list), dtype=torch.float32)
-        self.targets = torch.tensor(
-            np.array(y_list), dtype=torch.float32) if y_list is not None else None
+        self.targets = (
+            torch.tensor(np.array(y_list), dtype=torch.float32)
+            if y_list is not None
+            else None
+        )
 
     def __len__(self):
         return len(self.features)
@@ -28,17 +30,17 @@ class TabularDataset(Dataset):
             return self.features[idx]
 
 
-def TabularDataFactory(X, y=None, mode='Classification'):
+def TabularDataFactory(X, y=None, mode="Classification"):
     """
-        Creates list of instances for tabular data from pandas dataframe
+    Creates list of instances for tabular data from pandas dataframe
 
-        Input:
-            - X: pandas dataframe containing features
-            - y: pandas series containing targets
-            - mode: 'Classification' or 'Regression'
-        Output:
-            - X_list: list of instances
-            - y_list: list of targets
+    Input:
+        - X: pandas dataframe containing features
+        - y: pandas series containing targets
+        - mode: 'Classification' or 'Regression'
+    Output:
+        - X_list: list of instances
+        - y_list: list of targets
     """
 
     if y is None:
@@ -46,10 +48,11 @@ def TabularDataFactory(X, y=None, mode='Classification'):
             X = X.values
         return X.tolist()
     else:
-        if mode == 'Classification':
+        if mode == "Classification":
             # need to change to multi-class probability vector for classification
             encoder = OneHotEncoder(
-                sparse_output=False, categories='auto', handle_unknown='ignore')
+                sparse_output=False, categories="auto", handle_unknown="ignore"
+            )
             y = encoder.fit_transform(np.array(y).reshape(-1, 1))
 
         if type(X) != np.ndarray:
@@ -58,3 +61,59 @@ def TabularDataFactory(X, y=None, mode='Classification'):
             y = y.values
 
         return X.tolist(), y.tolist()
+
+
+def GraphDataFactory(X, y=None, mode="Classification", CFG=None):
+    """
+    Creates list of instances for tabular data from pandas dataframe
+
+    Input:
+        - X: pandas dataframe containing features
+        - y: pandas series containing targets
+        - mode: 'Classification' or 'Regression'
+    Output:
+        - X_list: list of instances
+        - y_list: list of targets
+    """
+
+    X_list = []
+    y_list = []
+
+    if y is None:
+
+        # group the data based on idx
+        for idx, group_x in X.groupby("idx"):
+            group_x = group_x.drop("idx", axis=1)
+            if type(group_x) != np.ndarray:
+                group_x = group_x.values
+            X_list.append([group_x])
+
+        return X_list
+    else:
+        if mode == "Classification":
+            y_idx = y["idx"]
+            # need to change to multi-class probability vector for classification
+            encoder = OneHotEncoder(
+                sparse_output=False, categories="auto", handle_unknown="ignore"
+            )
+            y = encoder.fit_transform(np.array(y[CFG["target"][0]]).reshape(-1, 1))
+            y = pd.DataFrame(y, columns=encoder.get_feature_names_out(["target"]))
+            y["idx"] = y_idx
+            CFG["target"] = encoder.get_feature_names_out(["target"])
+
+        for idx, group_x in X.groupby("idx"):
+            group_x = group_x.drop("idx", axis=1)
+            if type(group_x) != np.ndarray:
+                group_x = group_x.values
+            X_list.append([group_x])
+
+            if len(CFG["target"]) == 1:
+                group_y = y[y["idx"] == idx][CFG["target"][0]]
+            else:
+                group_y = y[y["idx"] == idx][CFG["target"]]
+
+            if type(group_y) != np.ndarray:
+                group_y = group_y.values
+            y_list.append(group_y)
+
+        return X_list, y_list
