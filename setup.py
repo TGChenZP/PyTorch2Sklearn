2bd0a910fa0b5627b48e0e016a17be166b827156
Lang (Ron) Chen <Ronstuckathome@gmail.com>
2024-09-07 12:02:33 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="PyTorch2Sklearn",  # Replace with your package name
-    version="0.2.1",
+    version="0.2.2",
     author="Lang Chen",
     author_email="ronchen6666@gmail.com",
     description="Refactoring PyTorch models into sklearn-like API",
